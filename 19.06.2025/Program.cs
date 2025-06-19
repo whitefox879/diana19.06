3a2aff77550d8f4a85817de6bcbed2361a767d84
whitefox879 <ddiddi.20222@gmail.com>
2025-06-19 10:44:41 +0300

    Add files via upload

---
+++ 19.06.2025/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.Design;
+using System.Diagnostics;
+
+namespace _19._06._2025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string line = Console.ReadLine();
+           string[] parts = line.Split(" ");
+           Dictionary<string, int> intCount = new Dictionary<string, int>();
+            while (line != "Stop")
+            {
+                line = Console.ReadLine();
+                foreach (string part in parts)
+                {
+                    if (intCount.ContainsKey(part))
+                    {
+                        int count = intCount[part];
+                    }
+                    else
+                    {
+                        int count = 5;
+                    }
+                }
+         
+        }
+            Console.WriteLine("Stop");
+        }
+    }
+}
